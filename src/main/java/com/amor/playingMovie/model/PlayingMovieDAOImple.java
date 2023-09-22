04ee734e4affef5d8bf12d50de92992cd4d43605
DESKTOP-C1T83CR\USER <ajy4606@naver.com>
2023-09-22 17:41:59 +0900

    JY

--- src/main/java/com/amor/playingMovie/model/PlayingMovieDAOImple.java
+++ src/main/java/com/amor/playingMovie/model/PlayingMovieDAOImple.java
@@ -1,5 +1,27 @@
 package com.amor.playingMovie.model;
 
+import java.util.*;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
 public class PlayingMovieDAOImple implements PlayingMovieDAO {
 
+	private SqlSessionTemplate sqlmap;
+	
+	public PlayingMovieDAOImple(SqlSessionTemplate sqlmap) {
+		super();
+		this.sqlmap = sqlmap;
+	}
+
+	@Override
+	public List<PlayingMovieDTO> playingMovieList(Map map) {
+		List<PlayingMovieDTO> lists=sqlmap.selectList("playingMovieList", map);
+		return lists;
+	}
+	
+	@Override
+	public int getTotalCnt() {
+		int count=sqlmap.selectOne("totalCnt");
+		return count;
+	}
 }
