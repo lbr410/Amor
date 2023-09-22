09d52c82fbd01ebd4c8411ca1aa085e713801ee9
BB\lbr41 <lbor3280@naver.com>
2023-09-22 17:42:36 +0900

    br

---
+++ src/main/java/com/amor/encryption/Encryption.java
@@ -0,0 +1,26 @@
+package com.amor.encryption;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Encryption {
+	public String pwdEncypt(String member_pwd) {
+		String MD5 = "";
+		
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(member_pwd.getBytes());
+			byte byteData[] = md.digest();
+			StringBuffer sb = new StringBuffer();
+			for(int i=0; i<byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			MD5 = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			MD5 = null;
+		}
+		
+		return MD5;
+	}
+}
--- src/main/java/com/amor/userController/SignUpController.java
+++ src/main/java/com/amor/userController/SignUpController.java
@@ -4,15 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member")
 public class SignUpController {
 	// 이용약관 페이지로 이동
-	@RequestMapping("member/terms.do")
+	@RequestMapping("/terms.do")
 	public String termsForm() {
 		return "user/member/terms";
 	}
 	
 	// 회원가입 페이지로 이동
-	@RequestMapping("member/signUp.do")
+	@RequestMapping("/signUp.do")
 	public String signUpForm() {
 		return "user/member/signUp";
 	}
